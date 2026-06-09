8a89a13aafe8d3a3d101e9453961d0e52ba72470
Rohit Mandal <rohit.percoidit@gmail.com>
2026-06-09 16:15:38 +0545

    Updated FramePageSpec code

--- tests/Alerts_Frame_Windows/framePage.spec.js
+++ tests/Alerts_Frame_Windows/framePage.spec.js
@@ -1,2 +1,22 @@
 const { test, expect } = require('@playwright/test');
-const FramesPage = require('../../pages/alerts-frames/FramesPage');
+const FramesPage = require('../../pages/Alerts_Frame_Windows/FramePage');
+
+
+test.describe('Frame - Test Cases', () => {
+
+    let framePage;
+
+    test.beforeEach(async ({ page }) => {
+        framePage = new FramesPage(page);
+
+        await framePage.openBrowser();
+        await page.pause();
+    });
+
+    test('Verify the Count', async () => {
+        await framePage.
+
+
+    });
+
+})
